26175f6ae6b4858ba8e91d363d99c6854de65ab7
Oleg Smirnov <sm-oleg1209@yandex.ru>
2020-09-05 22:10:57 +0300

    Add simple test case for EpsilonNFA intersection

--- tests/test_suite_example.py
+++ tests/test_suite_example.py
@@ -1,5 +1,7 @@
 from itertools import chain
 
+from pyformlang.finite_automaton import State, Symbol, EpsilonNFA
+from pyformlang.pda import State, Symbol
 from pygraphblas import Matrix
 
 
@@ -14,3 +16,37 @@ def test_matrix_product():
     matrix_actual = a @ b
     matrix_expected = create_matrix_from_array([[4, 4], [4, 4]])
     assert matrix_expected.iseq(matrix_actual)
+
+
+def test_pda_intersection():
+    q0 = State("q0")
+    q1 = State("q1")
+    q2 = State("q2")
+    q3 = State("q3")
+
+    a = Symbol("a")
+    b = Symbol("b")
+    c = Symbol("c")
+
+    epsilon_nfa_1 = EpsilonNFA(states={q0, q1, q2},
+                               input_symbols={a, b},
+                               start_state={q0},
+                               final_states={q1, q2})
+    epsilon_nfa_1.add_transition(q0, a, q1)
+    epsilon_nfa_1.add_transition(q1, b, q2)
+
+    epsilon_nfa_2 = EpsilonNFA(states={q0, q1, q3},
+                               input_symbols={a, c},
+                               start_state={q0},
+                               final_states={q1, q3})
+    epsilon_nfa_2.add_transition(q0, a, q1)
+    epsilon_nfa_2.add_transition(q1, c, q3)
+
+    expected_epsilon_nfa = EpsilonNFA(states={q0, q1},
+                                      input_symbols={a},
+                                      start_state={q0},
+                                      final_states={q1})
+    expected_epsilon_nfa.add_transition(q0, a, q1)
+
+    actual_intersection = epsilon_nfa_1.get_intersection(epsilon_nfa_2)
+    assert expected_epsilon_nfa.is_equivalent_to(actual_intersection)
